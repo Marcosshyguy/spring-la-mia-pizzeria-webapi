7db7e5fd38edf2a159539bc3b107439600767dcf
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-04-11 14:53:29 +0200

    Add: method get for single item

--- src/main/java/org/exercise/pizzeria/api/PizzaRestController.java
+++ src/main/java/org/exercise/pizzeria/api/PizzaRestController.java
@@ -14,16 +14,15 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/api/v1/pizzas")
+@RequestMapping("/api/pizzas")
 public class PizzaRestController {
     @Autowired
     private PizzaRepository pizzaRepository;
     @GetMapping
     public List<Pizza> getPizzas(@RequestParam(name = "type") Optional<String> keyword){
         List<Pizza> pizzas;
-
         if (keyword.isEmpty()) {
-            return pizzas = pizzaRepository.findAll(Sort.by("name"));
+            return pizzas = pizzaRepository.findAll();
         } else {
             return pizzas = pizzaRepository.findByNameContainingIgnoreCase(keyword.get());
         }
@@ -39,30 +38,31 @@ public class PizzaRestController {
         }
     }
 
-    @PostMapping
-    public Pizza create(@Valid @RequestBody Pizza pizza){
-        Pizza pizzaToCreate = new Pizza();
-        pizzaToCreate.setName(pizza.getName());
-        pizzaToCreate.setDescription(pizza.getDescription());
-        pizzaToCreate.setPrice(pizza.getPrice());
-        pizzaToCreate.setIngredientSet(pizza.getIngredientSet());
-        return pizzaRepository.save(pizzaToCreate);
-    }
-
-    @PutMapping("/{id}")
-    public Pizza update(@PathVariable("id") Integer id, @Valid @RequestBody Pizza pizza){
-        Pizza pizzaToUpdate = pizzaRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid pizza Id:" + id));
-
-        pizzaToUpdate.setName(pizza.getName());
-        pizzaToUpdate.setDescription(pizza.getDescription());
-        pizzaToUpdate.setPrice(pizza.getPrice());
-        pizzaToUpdate.setIngredientSet(pizza.getIngredientSet());
-        return pizzaRepository.save(pizzaToUpdate);
-    }
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Integer id){
-        pizzaRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid pizza Id: " + id));
-
-    }
+//
+//    @PostMapping
+//    public Pizza create(@Valid @RequestBody Pizza pizza){
+//        Pizza pizzaToCreate = new Pizza();
+//        pizzaToCreate.setName(pizza.getName());
+//        pizzaToCreate.setDescription(pizza.getDescription());
+//        pizzaToCreate.setPrice(pizza.getPrice());
+//        pizzaToCreate.setIngredientSet(pizza.getIngredientSet());
+//        return pizzaRepository.save(pizzaToCreate);
+//    }
+//
+//    @PutMapping("/{id}")
+//    public Pizza update(@PathVariable("id") Integer id, @Valid @RequestBody Pizza pizza){
+//        Pizza pizzaToUpdate = pizzaRepository.findById(id)
+//                .orElseThrow(() -> new IllegalArgumentException("Invalid pizza Id:" + id));
+//
+//        pizzaToUpdate.setName(pizza.getName());
+//        pizzaToUpdate.setDescription(pizza.getDescription());
+//        pizzaToUpdate.setPrice(pizza.getPrice());
+//        pizzaToUpdate.setIngredientSet(pizza.getIngredientSet());
+//        return pizzaRepository.save(pizzaToUpdate);
+//    }
+//    @DeleteMapping("/{id}")
+//    public void delete(@PathVariable("id") Integer id){
+//        pizzaRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid pizza Id: " + id));
+//
+//    }
 }
